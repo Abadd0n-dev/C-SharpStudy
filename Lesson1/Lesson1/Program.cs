15990ffa41fecd3981c3d5b6ae6a15c50d7edf58
Ruslan Kutaraev <ruslik0550@gmail.com>
2022-09-12 17:22:22 +0300

    All tasks for lesson 1 completed.

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,5 @@
+﻿//task 1 Создание переменной, а также вывод на экран приветствия и сегодняшней даты.
+Console.Write("What's your name?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}, today [{DateTime.Now}]");
+Console.Read();
